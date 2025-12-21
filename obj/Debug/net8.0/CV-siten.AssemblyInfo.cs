20f38f35a6deb20202d3466a9332431099f0afa3
LinoDeLuca <linodeluca2004@gmail.com>
2025-12-21 14:01:37 +0100

    städning av filer eller ngt

--- obj/Debug/net8.0/CV-siten.AssemblyInfo.cs
+++ obj/Debug/net8.0/CV-siten.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CV-siten")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02d1e9c340a36021a227641d941274278eba8bf2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8135de48230c42fea74dd4bc20e530b4dc8beaf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CV-siten")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CV-siten")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
