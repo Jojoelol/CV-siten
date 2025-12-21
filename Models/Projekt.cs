2be109ad31e63a52762a4d2cb3bbffc2ff9c81d2
Jojoelol <arrebackjoel@gmail.com>
2025-12-21 15:43:26 +0100

    fix med models

---
+++ Models/Projekt.cs
@@ -0,0 +1,13 @@
+﻿namespace CV_siten.Models
+{
+    public class Projekt
+    {
+        public int Id { get; set; }
+        public string Projektnamn { get; set; }
+        public string Beskrivning { get; set; }
+        public DateTimeOffset Startdatum { get; set; }
+        public DateTimeOffset Slutdatum { get; set; }
+        public string Typ { get; set; }
+        public string Status { get; set; }
+    }
+}
