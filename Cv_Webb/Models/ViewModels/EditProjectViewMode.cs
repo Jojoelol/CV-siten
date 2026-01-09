fa5d7af109f2443768b92581a676cb4bdda49b0c
LinoDeLuca <linodeluca2004@gmail.com>
2026-01-09 12:22:39 +0100

    Validering för projekt editering

--- Cv_Webb/Models/ViewModels/EditProjectViewMode.cs
+++ Cv_Webb/Models/ViewModels/EditProjectViewMode.cs
@@ -11,17 +11,23 @@ namespace CV_siten.Models.ViewModels // Kontrollera att detta matchar din mappst
         [Display(Name = "Projektnamn")]
         public string ProjectName { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "Beskrivning måste anges")]
         [Display(Name = "Beskrivning")]
         public string? Description { get; set; }
 
+        [Required(ErrorMessage = "Startdatum måste anges")]
         [Display(Name = "Startdatum")]
         public DateTimeOffset StartDate { get; set; }
 
+        [Required(ErrorMessage = "Slutdatum måste anges")]
         [Display(Name = "Slutdatum")]
         public DateTimeOffset? EndDate { get; set; }
 
-        public string? Type { get; set; }
-        public string? Status { get; set; }
+        [Required(ErrorMessage = "Typ måste anges")]
+        public string Type { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Stauts måste anges")]
+        public string Status { get; set; } = string.Empty;
 
         // För att behålla befintliga filer
         public string? ImageUrl { get; set; }
